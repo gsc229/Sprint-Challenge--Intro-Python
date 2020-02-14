31ea016cc45a2b5e2f09ecbb51800ae6878302b2
Greg <gsc229@gmail.com>
2020-02-14 11:22:25 -0500

    oop2 passing 5 tests

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -3,12 +3,15 @@
 # Also change it so the num_wheels defaults to 4 if not specified when the
 # object is constructed.
 
+
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    def __init__(self, num_wheels=4):
         self.num_wheels = num_wheels
 
-    # TODO
+    def drive(self):
+        return "vroooom"
 
+    # TODO
 
 # Subclass Motorcycle from GroundVehicle.
 #
@@ -16,6 +19,15 @@ class GroundVehicle():
 # of wheels to 2 by passing that to the constructor of its superclass.
 #
 # Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
+
+
+class Motorcycle(GroundVehicle):
+    def __init__(self):
+        super().__init__(num_wheels=2)
+
+    def drive(self):
+        return "BRAAAP!!"
+
 
 # TODO
 
@@ -28,5 +40,7 @@ vehicles = [
 ]
 
 # Go through the vehicles list and print the result of calling drive() on each.
+for v in vehicles:
+    print(v.drive())
 
 # TODO
